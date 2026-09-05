e14e45b7dc2bb966e8c5543d4663a27a2b17d6c3
LaBricolle <charly.his74@gmail.com>
2026-09-05 15:35:42 +0200

    FranceInfo URL Switch

--- src/lib/feeds.ts
+++ src/lib/feeds.ts
@@ -2,7 +2,7 @@
 // Actu générale, plusieurs sensibilités éditoriales pour un scoring plus robuste
 // (un sujet repris par plusieurs sources différentes = plus susceptible d'être "gros").
 export const FEEDS: { name: string; url: string }[] = [
-  { name: 'Franceinfo', url: 'https://www.francetvinfo.fr/titres.rss' },
+  { name: 'Franceinfo', url: 'https://www.franceinfo.fr/titres.rss' },
   { name: 'Le Monde', url: 'https://www.lemonde.fr/rss/une.xml' },
   { name: 'Le Figaro', url: 'https://www.lefigaro.fr/rss/figaro_actualites.xml' },
   { name: 'Libération', url: 'https://www.liberation.fr/arc/outboundfeeds/rss/?outputType=xml' },
